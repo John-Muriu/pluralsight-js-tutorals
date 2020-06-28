fb3b1dc84d8804c161d46b522e74497bada110a5
John-Muriu <johnmuriw@gmail.com>
2020-06-28 17:22:24 +0300

    opartors

--- home.js
+++ home.js
@@ -5,7 +5,7 @@
 // showMessage('Welcome');
 
 //types and operators{ symbols that add functionalitions to our code}
-
+// numbers and operators
 let price = 67;
 // price = price + 2;
 price = price * 3;
