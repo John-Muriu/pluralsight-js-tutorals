e3cc9068d12153d3b2376c1a5f3f0454ed3937dd
John-Muriu <johnmuriw@gmail.com>
2020-06-28 18:48:48 +0300

    truthy and falsy

--- home.js
+++ home.js
@@ -63,6 +63,6 @@ if (price !== 40) {
 
 
 // // we can fix this  by rounding of our values
-if (+(1.1 + 2.3).toFixed(2) !== 2.4) {
+if (+(1.1 + 1.3).toFixed(2) === 2.4) {
     showMessage("True");
 }
