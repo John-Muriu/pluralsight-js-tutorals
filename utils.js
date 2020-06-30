e5db8ba76c6f62a3c73fbfa3defaa16cc8f60cf6
John-Muriu <johnmuriw@gmail.com>
2020-06-30 19:06:46 +0300

    Regular auto-commit at 19:06:45 on 30/06/2020

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 };
+
+function changePercentOff(percentOff) {
+    document.getElementById('percentOff').textContent = percentOff;
+};
